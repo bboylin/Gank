a567492ba49b87eee69f2979c5a4f44b28a21867
bboylin <bboylin24@gmail.com>
2016-12-31 02:33:41 +0800

    完成分享和图片保存

--- app/src/main/java/com/bboylin/gank/Event/GankClickEvent.java
+++ app/src/main/java/com/bboylin/gank/Event/GankClickEvent.java
@@ -1,17 +1,19 @@
 package com.bboylin.gank.Event;
 
+import com.bboylin.gank.Data.Entity.Gank;
+
 /**
  * Created by lin on 2016/11/12.
  */
 
-public class UrlClickEvent {
-    public String url;
+public class GankClickEvent {
+    public Gank mGank;
     public int type;
     public static final int TEXT = 0;
     public static final int IMAGE = 1;
 
-    public UrlClickEvent(String url, int type) {
-        this.url = url;
+    public GankClickEvent(Gank mGank, int type) {
+        this.mGank = mGank;
         this.type = type;
     }
 }
---
+++ app/src/main/java/com/bboylin/gank/UI/Widget/ShareBottomDialog.java
@@ -0,0 +1,8 @@
+package com.bboylin.gank.UI.Widget;
+
+/**
+ * Created by lin on 2016/12/30.
+ */
+
+public class ShareBottomDialog {
+}
